ea010c6735e2b56a6cc6a6b200e142f42eb0a73a
Drew DeCarme <drewdecarme@gmail.com>
2020-06-03 09:10:53 -0400

    update data structure for coupons

--- packages/websites/mindful-movement/gatsby-config.js
+++ packages/websites/mindful-movement/gatsby-config.js
@@ -53,6 +53,14 @@ module.exports = {
         icon: `static/favicon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-stripe`,
+      options: {
+        objects: ["Product"],
+        secretKey: process.env.HTC_STRIPE_SECRET_KEY,
+        downloadFiles: true,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- packages/websites/mindful-movement/gatsby-node.js
+++ packages/websites/mindful-movement/gatsby-node.js
@@ -1,10 +1,19 @@
 const contentful = require("contentful");
 const stripe = require("stripe")(process.env.HTC_STRIPE_SECRET_KEY);
+const path = require("path");
 
 const contentfulClient = contentful.createClient({
   space: process.env.HTC_MINDFUL_MOVEMENT_CONTENTFUL_SPACE_ID,
   accessToken: process.env.HTC_MINDFUL_MOVEMENT_CONTENTFUL_ACCESS_TOKEN,
 });
+
+const reduceProductsToSingleProductById = (products, productId) =>
+  products.reduce((accum, product) => {
+    if (productId === product.id) {
+      return product;
+    }
+    return accum;
+  }, {});
 
 exports.sourceNodes = async ({
   actions,
@@ -27,32 +36,54 @@ exports.sourceNodes = async ({
     }),
   ]);
 
+  console.log("> creating stripe prices...");
+  stripePricesData.data.forEach((price) => {
+    const priceNode = {
+      ...price,
+      product: reduceProductsToSingleProductById(
+        stripeProductsData.data,
+        price.product
+      ),
+      // metadata: {
+      //   ...defaultPriceMetadata,
+      //   ...stripe.metadata,
+      // },
+    };
+    const nodeMeta = {
+      parent: null,
+      children: [],
+      internal: {
+        type: `StripePrice`,
+        mediaType: `text/html`,
+        content: JSON.stringify(priceNode),
+        contentDigest: createContentDigest(priceNode),
+      },
+    };
+    createNode({
+      ...priceNode,
+      ...nodeMeta,
+    });
+  });
+  console.log("> creating stripe prices... complete.");
+
+  console.log("> creating base products and prices...");
   contentfulProductInformation.items.forEach(
     ({ fields: contentfulProduct }) => {
-      const resolvedProductId =
-        process.env.NODE_ENV === "production"
-          ? contentfulProduct.stripeProductIdProd
-          : contentfulProduct.stripeProductId;
-      const resolvedPriceId =
-        process.env.NODE_ENV === "production"
-          ? contentfulProduct.stripePriceIdProd
-          : contentfulProduct.stripePriceId;
+      // get the product id from contentful
+      const productKeyId = `stripeProductId__${process.env.NODE_ENV}`;
+      const resolvedProductId = contentfulProduct[productKeyId];
 
-      console.log(resolvedProductId, resolvedPriceId);
+      const stripeProduct = reduceProductsToSingleProductById(
+        stripeProductsData.data,
+        resolvedProductId
+      );
 
       const stripePrice = stripePricesData.data.reduce((accum, price) => {
         if (
           price.product === resolvedProductId &&
-          price.id === resolvedPriceId
+          price.metadata.type === "base"
         ) {
           return price;
-        }
-        return accum;
-      }, {});
-
-      const stripeProduct = stripeProductsData.data.reduce((accum, product) => {
-        if (resolvedProductId === product.id) {
-          return product;
         }
         return accum;
       }, {});
@@ -72,58 +103,140 @@ exports.sourceNodes = async ({
       createNode({
         ...contentfulProduct,
         stripeProductId: resolvedProductId,
-        stripePriceId: resolvedPriceId,
+        stripePriceId: stripePrice.id,
         ...nodeMeta,
         stripePrice,
         stripeProduct,
       });
     }
   );
+  console.log("> creating base products and prices... complete.");
 
-  console.log("----------     Success!   --------------");
+  console.log("> creating coupons from price meta data...");
+  const coupons = stripePricesData.data.reduce((accum, price) => {
+    if (
+      !accum.includes(price.metadata.slug) &&
+      typeof price.metadata.slug !== "undefined"
+    ) {
+      return [...accum, price.metadata.slug];
+    }
+    return accum;
+  }, []);
+
+  coupons.forEach((couponSlug) => {
+    const nodeMeta = {
+      id: createNodeId(couponSlug),
+      parent: null,
+      children: [],
+      internal: {
+        type: `StripeCouponFromMeta`,
+        mediaType: `text/html`,
+        content: JSON.stringify(couponSlug),
+        contentDigest: createContentDigest(couponSlug),
+      },
+    };
+
+    createNode({
+      slug: couponSlug,
+      ...nodeMeta,
+    });
+  });
+
+  console.log("creating coupons from price meta data... complete.");
   console.log("----------------------------------------");
 };
 
-// exports.createPages = async ({ graphql, actions: { createPage } }) => {
-//   // **Note:** The graphql function call returns a Promise
-//   // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise for more info
-//   const result = await graphql(`
-//     query {
-//       contentfulPageHome {
-//         pricingTitle
-//         pricingDescription {
-//           pricingDescription
-//         }
-//       }
-//       allStripeProductAndPrice {
-//         nodes {
-//           name
-//           features
-//           color
-//           order
-//           logo {
-//             fields {
-//               file {
-//                 url
-//               }
-//             }
-//           }
-//           stripePriceId
-//           stripeProductId
-//           stripePrice {
-//             unit_amount
-//           }
-//           couponOg
-//         }
-//       }
-//     }
-//   `);
+exports.createPages = async ({ graphql, actions: { createPage } }) => {
+  const result = await graphql(`
+    query {
+      allStripeCouponFromMeta {
+        nodes {
+          slug
+        }
+      }
+      allContentfulPackage {
+        nodes {
+          stripeProductId__development
+          stripeProductId__production
+          order
+          name
+          logo {
+            file {
+              url
+            }
+          }
+          features
+          contentfulTitle
+          color
+        }
+      }
+      allStripePrice {
+        nodes {
+          id
+          unit_amount
+          metadata {
+            slug
+            type
+          }
+          product {
+            id
+          }
+        }
+      }
+    }
+  `);
 
-//   createPage({
-//     path: "/coupon",
-//     component: path.resolve(`./src/templates/coupon.tsx`),
-//     context: {
-//       data: result,
-//     },
-//   });
-// };
+  // Coupons
+  result.data.allStripeCouponFromMeta.nodes.forEach((coupon) => {
+    const packages = result.data.allContentfulPackage.nodes.map(
+      (contentfulPackage) => {
+        const productId =
+          contentfulPackage[`stripeProductId__${process.env.NODE_ENV}`];
+        const {
+          stripeProductId__development,
+          stripeProductId__production,
+          ...productMetaData
+        } = contentfulPackage;
+
+        return {
+          productId,
+          ...productMetaData,
+          basePrice: result.data.allStripePrice.nodes.reduce((accum, price) => {
+            if (
+              price.product.id === productId &&
+              price.metadata.type === "base"
+            ) {
+              return price;
+            }
+            return accum;
+          }, {}),
+          couponPrice: result.data.allStripePrice.nodes.reduce(
+            (accum, price) => {
+              if (
+                price.product.id === productId &&
+                price.metadata.type === "coupon" &&
+                price.metadata.slug === coupon.slug
+              ) {
+                return price;
+              }
+              return accum;
+            },
+            {}
+          ),
+        };
+      }
+    );
+
+    console.log(packages);
+
+    createPage({
+      path: `/${coupon.slug}`,
+      component: path.resolve(`./src/templates/coupon.tsx`),
+      context: {
+        data: {
+          packages,
+        },
+      },
+    });
+  });
+};
