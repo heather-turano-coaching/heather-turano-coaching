d59528b4cff627921a5521955d4f43cdbc945332
Drew DeCarme <drewdecarme@gmail.com>
2020-06-22 13:53:57 -0400

    add dynamic links

--- packages/websites/mindful-movement/src/components/Layout.tsx
+++ packages/websites/mindful-movement/src/components/Layout.tsx
@@ -4,7 +4,7 @@ import {
   FooterText,
 } from "@heather-turano-coaching/components";
 import { makeColor } from "@heather-turano-coaching/design-system";
-import { Link } from "gatsby";
+import { Link, graphql, useStaticQuery } from "gatsby";
 import React, { FC } from "react";
 
 import { NavBar } from "./NavBar";
@@ -12,94 +12,112 @@ import NavBarLinkAnchor from "./NavBarLinkAnchor";
 import { NavBarSection } from "./NavBarSection";
 import { NavLogo } from "./NavLogo";
 
-export const Layout: FC = ({ children }) => (
-  <div id="htc-root">
-    <NavBar>
-      <NavBarSection>
-        <NavLogo />
-      </NavBarSection>
-      <NavBarSection component="ul" collapse>
-        <NavBarLinkAnchor href="/#about">About</NavBarLinkAnchor>
-        <NavBarLinkAnchor href="/#who-we-are">Who we are</NavBarLinkAnchor>
-        <NavBarLinkAnchor href="/#pricing">Pricing</NavBarLinkAnchor>
-        <NavBarLinkAnchor href="/#schedule">Schedule</NavBarLinkAnchor>
-      </NavBarSection>
-    </NavBar>
-    {children}
-    <Footer>
-      <FooterSection title="Mindful Movement 100">
-        <FooterText>
-          <span>
-            Copyright © 2020, Heather Turano Coaching, LLC, All Rights Reserved.
-            Mindful Movement 100 is a trademark of Heather Turano Coaching, LLC.
-            The use of the trademark Mindful Movement 100 outside the bounds of
-            this website requires exclusive written consent from Heather Turano
-            Coaching, LLC."
-          </span>
-        </FooterText>
-        <FooterText>
-          <span>This website was designed and developed by &nbsp;</span>
-          <div>
+export const Layout: FC = ({ children }) => {
+  const {
+    allMarkdownRemark: { nodes: disclosurePages },
+  } = useStaticQuery<{
+    allMarkdownRemark: {
+      nodes: { frontmatter: { slug: string; title: string } }[];
+    };
+  }>(graphql`
+    {
+      allMarkdownRemark(
+        sort: { fields: frontmatter___order }
+        filter: { frontmatter: { slug: { ne: null } } }
+      ) {
+        nodes {
+          frontmatter {
+            slug
+            title
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    <div id="htc-root">
+      <NavBar>
+        <NavBarSection>
+          <NavLogo />
+        </NavBarSection>
+        <NavBarSection component="ul" collapse>
+          <NavBarLinkAnchor href="/#about">About</NavBarLinkAnchor>
+          <NavBarLinkAnchor href="/#who-we-are">Who we are</NavBarLinkAnchor>
+          <NavBarLinkAnchor href="/#pricing">Pricing</NavBarLinkAnchor>
+          <NavBarLinkAnchor href="/#schedule">Schedule</NavBarLinkAnchor>
+        </NavBarSection>
+      </NavBar>
+      {children}
+      <Footer>
+        <FooterSection title="Mindful Movement 100">
+          <FooterText>
+            <span>
+              Copyright © 2020, Heather Turano Coaching, LLC, All Rights
+              Reserved. Mindful Movement 100 is a trademark of Heather Turano
+              Coaching, LLC. The use of the trademark Mindful Movement 100
+              outside the bounds of this website requires exclusive written
+              consent from Heather Turano Coaching, LLC."
+            </span>
+          </FooterText>
+          <FooterText>
+            <span>This website was designed and developed by &nbsp;</span>
+            <div>
+              <a
+                href="https://github.com/drewdecarme"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{
+                  display: "inlineBlock",
+                  textDecorationColor: `${makeColor({ fixed: "light" })}`,
+                  color: `${makeColor({ fixed: "light" })}`,
+                }}
+              >
+                Drew DeCarme
+              </a>
+            </div>
+          </FooterText>
+        </FooterSection>
+        <FooterSection title="Privacy &amp; Security">
+          <FooterText>
+            Mindful Movement 100 takes your privacy very seriously. Use the
+            links below to learn more about our policies
+          </FooterText>
+          <ul>
+            {disclosurePages.map((page) => (
+              <li>
+                <FooterText>
+                  <Link to={page.frontmatter.slug}>
+                    {page.frontmatter.title}
+                  </Link>
+                </FooterText>
+              </li>
+            ))}
+          </ul>
+        </FooterSection>
+        <FooterSection title="Sponsors">
+          <FooterText>
+            <div>Heather Turano</div>
             <a
-              href="https://github.com/drewdecarme"
+              href="https://heatherturanocoaching.com"
               target="_blank"
               rel="noopener noreferrer"
-              style={{
-                display: "inlineBlock",
-                textDecorationColor: `${makeColor({ fixed: "light" })}`,
-                color: `${makeColor({ fixed: "light" })}`,
-              }}
             >
-              Drew DeCarme
+              Heather Turano Coaching, LLC
             </a>
-          </div>
-        </FooterText>
-      </FooterSection>
-      <FooterSection title="Privacy &amp; Security">
-        <FooterText>
-          Mindful Movement 100 takes your privacy very seriously. Use the links
-          below to learn more about our policies
-        </FooterText>
-        <ul>
-          <li>
-            <FooterText>
-              <Link to="/security-overview">Summary</Link>
-            </FooterText>
-          </li>
-          <li>
-            <FooterText>
-              <Link to="/privacy-policy">Privacy Policy</Link>
-            </FooterText>
-          </li>
-          <li>
-            <FooterText>
-              <Link to="/terms-and-conditions">Terms and Conditions</Link>
-            </FooterText>
-          </li>
-        </ul>
-      </FooterSection>
-      <FooterSection title="Sponsors">
-        <FooterText>
-          <div>Heather Turano</div>
-          <a
-            href="https://heatherturanocoaching.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Heather Turano Coaching, LLC
-          </a>
-        </FooterText>
-        <FooterText>
-          <div>Amanda Rock</div>
-          <a
-            href="http://www.thecompassconsultinggroup.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            The Compass Consulting Group
-          </a>
-        </FooterText>
-      </FooterSection>
-    </Footer>
-  </div>
-);
+          </FooterText>
+          <FooterText>
+            <div>Amanda Rock</div>
+            <a
+              href="http://www.thecompassconsultinggroup.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              The Compass Consulting Group
+            </a>
+          </FooterText>
+        </FooterSection>
+      </Footer>
+    </div>
+  );
+};
