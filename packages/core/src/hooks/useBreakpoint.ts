a1081ba998ae166a868853f428813a561d232699
Drew DeCarme <drewdecarme@gmail.com>
2020-09-24 13:17:57 -0400

    fix: Remove dependency on local htc packages

--- packages/core/src/hooks/useBreakpoint.ts
+++ packages/core/src/hooks/useBreakpoint.ts
@@ -1,8 +1,26 @@
-import {
-  ResponsiveBreakpoints,
-  responsiveBreakpoints
-} from "@heather-turano-coaching/design-system";
 import { useEffect, useState } from "react";
+
+export type ResponsiveDeviceTypes =
+  | "phone"
+  | "phoneMd"
+  | "phoneLg"
+  | "tabletPortrait"
+  | "tabletLandscape"
+  | "laptop"
+  | "desktop"
+  | "4K";
+export type ResponsiveBreakpoints = { [key in ResponsiveDeviceTypes]: number };
+
+export const responsiveBreakpoints: ResponsiveBreakpoints = {
+  phone: 320,
+  phoneMd: 375,
+  phoneLg: 425,
+  tabletPortrait: 768,
+  tabletLandscape: 1024,
+  laptop: 1024,
+  desktop: 1440,
+  "4K": 2560
+};
 
 export const useBreakpoints = (
   userDefinedBreakpoints?: ResponsiveBreakpoints
