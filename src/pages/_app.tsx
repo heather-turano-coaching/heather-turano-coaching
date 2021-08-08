ac671ca865359cd4c6b64c736e1b01b4fbc80a86
Drew DeCarme <drewdecarme@gmail.com>
2021-08-08 13:35:48 -0400

    fix: Updates matomo instance

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,12 +19,9 @@ export default function MyApp({
   }, []);
 
   useEffect(() => {
-    if (
-      process.env.NODE_ENV === "production" &&
-      typeof window !== "undefined"
-    ) {
+    if (process.env.NODE_ENV === "production") {
       router.events.on("routeChangeStart", (url) => {
-        if (window._paq) {
+        if (window && window._paq) {
           window._paq.push(["setCustomUrl", url]);
           window._paq.push(["setDocumentTitle", document.title]);
           window._paq.push(["trackPageView"]);
@@ -57,21 +54,18 @@ export default function MyApp({
           <script
             dangerouslySetInnerHTML={{
               __html: `
-                  const _paq = (window._paq = window._paq || []);
-                  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-                  _paq.push(["trackPageView"]);
-                  _paq.push(["enableLinkTracking"]);
-                  (function () {
-                    _paq.push(["setTrackerUrl", "https://heatherturanocoaching.matomo.cloud/matomo.php"]);
-                    _paq.push(["setSiteId", "1"]);
-                    const d = document,
-                      g = d.createElement("script"),
-                      s = d.getElementsByTagName("script")[0];
-                    g.type = "text/javascript";
-                    g.async = true;
-                    g.src = "//cdn.matomo.cloud/heatherturanocoaching.matomo.cloud/matomo.js";
-                    s.parentNode.insertBefore(g, s);
-                  })();
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="https://heatherturanocoaching.matomo.cloud/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.src='//cdn.matomo.cloud/heatherturanocoaching.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+
           `
             }}
           />
