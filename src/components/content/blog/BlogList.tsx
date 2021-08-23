36afd70cf54033f0216eee81636cc22a7546ddc3
Drew DeCarme <drewdecarme@gmail.com>
2021-08-23 18:00:21 -0400

    fix: Update blog grid

--- src/components/content/blog/BlogList.tsx
+++ src/components/content/blog/BlogList.tsx
@@ -1,4 +1,4 @@
-import { makeRem, makeTabletStyles } from "@htc/theme";
+import { makeDesktopStyles, makeRem, makeTabletStyles } from "@htc/theme";
 import { PostOrPage } from "@tryghost/content-api";
 import React, { forwardRef } from "react";
 import styled, { css } from "styled-components";
@@ -18,10 +18,23 @@ const SyledBlogList = styled.div`
     }
 
     ${makeTabletStyles(theme)} {
+      display: flex;
+      flex-wrap: wrap;
       & > * {
-        width: 54%;
-        margin-left: auto;
-        margin-right: auto;
+        width: ${`calc(50% - ${makeRem(2 * 2 * 8)})`};
+        margin-left: ${makeRem(16)};
+        margin-right: ${makeRem(16)};
+        margin-bottom: ${makeRem(80)};
+      }
+    }
+
+    ${makeDesktopStyles(theme)} {
+      display: flex;
+      justify-content: center;
+      & > * {
+        width: ${`calc(33.3% - ${makeRem(3 * 2 * 8)})`};
+        margin-left: ${makeRem(16)};
+        margin-right: ${makeRem(16)};
         margin-bottom: ${makeRem(80)};
       }
     }
