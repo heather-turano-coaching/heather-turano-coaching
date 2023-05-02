c79ee05eca1cb5cd59ea6a8295e3a173d05fa2ba
Drew DeCarme <drewdecarme@gmail.com>
2023-05-02 10:18:59 -0400

    mobile

--- src/features/promotion/Promotion.page.tsx
+++ src/features/promotion/Promotion.page.tsx
@@ -4,11 +4,27 @@ import React from "react";
 
 import { withPromotionPageLayout } from "./Promotion.layout";
 import { IPromotion } from "@htc/lib/contentful/contentful.types";
+import {
+  Button,
+  Container,
+  ContentfulRichText,
+  Input,
+  Title
+} from "@htc/components/atomic";
+import styled from "styled-components";
+import { makeFontWeight, makeRem } from "@htc/theme";
 
 export type PromotionPageProps = {
   promotion: IPromotion;
 };
 
+const SDiv = styled("div")`
+  padding: ${makeRem(32)};
+
+  strong {
+    font-weight: ${makeFontWeight("black")};
+  }
+`;
 export const PromotionPage: FeaturePageComponent<PromotionPageProps> = ({
   promotion
 }) => {
@@ -19,15 +35,40 @@ export const PromotionPage: FeaturePageComponent<PromotionPageProps> = ({
   }
 
   return (
-    <form method="POST" action={promotion.fields.audiencefulPostActionUrl}>
-      <input
-        name="email"
-        type="email"
-        id="email"
-        placeholder="youremail@yourdomain.com"
-        required
-      />
-    </form>
+    <SDiv>
+      <Container>
+        <Title size="md">{promotion.fields.title}</Title>
+        <br />
+        <ContentfulRichText
+          richText={promotion.fields.description}
+          copyProps={{
+            variant: "paragraph"
+          }}
+        />
+        <br />
+        <form method="POST" action={promotion.fields.audiencefulPostActionUrl}>
+          <Input
+            label="Email Address"
+            type="text"
+            name="email"
+            id="email"
+            placeholder="youremail@yourdomain.com"
+            required
+          />
+          <div
+            style={{
+              position: "absolute",
+              left: -5000
+            }}
+            aria-hidden="true"
+          >
+            <input type="text" name="b28-ft" tabIndex={-1} value="" />
+          </div>
+          <br />
+          <Button type="submit" label="Sign up!" styleType="secondary" />
+        </form>
+      </Container>
+    </SDiv>
   );
 };
 
