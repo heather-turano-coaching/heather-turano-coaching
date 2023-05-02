d9a23f7593260afbfd315a89d86db8b489710adf
Drew DeCarme <drewdecarme@gmail.com>
2023-05-02 09:51:15 -0400

    storybook migration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,5 @@
 module.exports = {
-  extends: [
-    "plugin:@typescript-eslint/recommended",
-    "next/core-web-vitals",
-    "prettier"
-  ],
+  extends: ["plugin:@typescript-eslint/recommended", "next/core-web-vitals", "prettier", "plugin:storybook/recommended", "plugin:storybook/recommended"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
@@ -17,19 +13,16 @@ module.exports = {
     // Typescript Overrides
     "@typescript-eslint/explicit-function-return-type": 0,
     "@typescript-eslint/explicit-module-boundary-types": 0,
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
-      { argsIgnorePattern: "^_", ignoreRestSiblings: true }
-    ],
+    "@typescript-eslint/no-unused-vars": ["warn", {
+      argsIgnorePattern: "^_",
+      ignoreRestSiblings: true
+    }],
     // Next Overrides
     "@next/next/no-img-element": 0,
     // Import Overrides
     "import/no-anonymous-default-export": 0,
-    "import/no-extraneous-dependencies": [
-      "error",
-      {
-        devDependencies: ["src/**/*.spec.ts*", "src/**/*.stories.tsx"]
-      }
-    ]
+    "import/no-extraneous-dependencies": ["error", {
+      devDependencies: ["src/**/*.spec.ts*", "src/**/*.stories.tsx"]
+    }]
   }
 };
