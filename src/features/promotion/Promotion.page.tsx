4654b9eb2bd1ca68066441f704ec6ad3bf8e75f3
Drew DeCarme <drewdecarme@gmail.com>
2023-05-02 10:25:59 -0400

    desktop page

--- src/features/promotion/Promotion.page.tsx
+++ src/features/promotion/Promotion.page.tsx
@@ -11,12 +11,39 @@ import {
   Input,
   Title
 } from "@htc/components/atomic";
-import styled from "styled-components";
-import { makeFontWeight, makeRem } from "@htc/theme";
+import styled, { css } from "styled-components";
+import {
+  makeDesktopStyles,
+  makeFontWeight,
+  makeMobileStyles,
+  makeRem
+} from "@htc/theme";
 
 export type PromotionPageProps = {
   promotion: IPromotion;
 };
+
+const SGrid = styled("div")`
+  ${({ theme }) => css`
+    ${makeDesktopStyles(theme)} {
+      display: grid;
+      grid-template-columns: ${makeRem(500)} 1fr;
+    }
+  `}
+`;
+
+const SLeft = styled("div")`
+  ${({ theme }) => css`
+    ${makeMobileStyles(theme)} {
+      display: none;
+    }
+  `}
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
 
 const SDiv = styled("div")`
   padding: ${makeRem(32)};
@@ -35,40 +62,51 @@ export const PromotionPage: FeaturePageComponent<PromotionPageProps> = ({
   }
 
   return (
-    <SDiv>
-      <Container>
-        <Title size="md">{promotion.fields.title}</Title>
-        <br />
-        <ContentfulRichText
-          richText={promotion.fields.description}
-          copyProps={{
-            variant: "paragraph"
-          }}
-        />
-        <br />
-        <form method="POST" action={promotion.fields.audiencefulPostActionUrl}>
-          <Input
-            label="Email Address"
-            type="text"
-            name="email"
-            id="email"
-            placeholder="youremail@yourdomain.com"
-            required
-          />
-          <div
-            style={{
-              position: "absolute",
-              left: -5000
-            }}
-            aria-hidden="true"
-          >
-            <input type="text" name="b28-ft" tabIndex={-1} value="" />
-          </div>
+    <SGrid>
+      <SDiv>
+        <Container>
+          <Title size="md">{promotion.fields.title}</Title>
           <br />
-          <Button type="submit" label="Sign up!" styleType="secondary" />
-        </form>
-      </Container>
-    </SDiv>
+          <ContentfulRichText
+            richText={promotion.fields.description}
+            copyProps={{
+              variant: "paragraph"
+            }}
+          />
+          <br />
+          <form
+            method="POST"
+            action={promotion.fields.audiencefulPostActionUrl}
+          >
+            <Input
+              label="Email Address"
+              type="text"
+              name="email"
+              id="email"
+              placeholder="youremail@yourdomain.com"
+              required
+            />
+            <div
+              style={{
+                position: "absolute",
+                left: -5000
+              }}
+              aria-hidden="true"
+            >
+              <input type="text" name="b28-ft" tabIndex={-1} value="" />
+            </div>
+            <br />
+            <Button type="submit" label="Sign up!" styleType="secondary" />
+          </form>
+        </Container>
+      </SDiv>
+      <SLeft>
+        <img
+          src={promotion.fields.image.fields.file.url}
+          alt={promotion.fields.image.fields.title}
+        />
+      </SLeft>
+    </SGrid>
   );
 };
 
