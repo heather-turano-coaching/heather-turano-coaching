19d032ab48e88ac9a9301f6f7597e3ed8322f018
Drew DeCarme <20549969+drewdecarme@users.noreply.github.com>
2023-05-06 08:15:04 -0400

    fix: Removes dynamic styling and utilizes `next/script` instead of `Head` component (#28)

--- src/features/free-consult/FreeConsultPage.tsx
+++ src/features/free-consult/FreeConsultPage.tsx
@@ -1,12 +1,19 @@
-/* eslint-disable @next/next/no-sync-scripts */
 import { FeaturePageComponent } from "@htc/features/page";
 import { makeRem } from "@htc/theme";
 import { NextSeo } from "next-seo";
-import Head from "next/head";
 import React from "react";
-import { css } from "styled-components";
+import styled from "styled-components";
 
 import { withFreeConsultPageLayout } from "./FreeConsult.layout";
+import Script from "next/script";
+
+const SDiv = styled("div")`
+  height: ${makeRem(1000)};
+
+  .calendly-inline-widget {
+    height: 100% !important;
+  }
+`;
 
 export const FreeConsultPage: FeaturePageComponent = () => {
   return (
@@ -15,15 +22,7 @@ export const FreeConsultPage: FeaturePageComponent = () => {
         title="Free 30-Minute Consultation"
         description="Sign up for a free 30 minute consultation to understand and learn how Heather can help you."
       />
-      <div
-        css={css`
-          height: ${makeRem(1000)};
-
-          .calendly-inline-widget {
-            height: 100% !important;
-          }
-        `}
-      >
+      <SDiv>
         <div
           className="calendly-inline-widget"
           data-url="https://calendly.com/heather-turano-coaching/30-minute-introductory-consult?text_color=4a4a4a&primary_color=4e8588"
@@ -32,10 +31,8 @@ export const FreeConsultPage: FeaturePageComponent = () => {
             height: 630
           }}
         />
-        <Head>
-          <script src="https://assets.calendly.com/assets/external/widget.js" />
-        </Head>
-      </div>
+        <Script src="https://assets.calendly.com/assets/external/widget.js" />
+      </SDiv>
     </>
   );
 };
